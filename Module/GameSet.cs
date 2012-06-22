c92a6e82ce402fb4423d6c2d208d1b17406a55ba
xavier.lite <xavier.lite@gmail.com>
2012-06-22 21:05:11 +0800

    Add GameSet Class

---
+++ Module/GameSet.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Ascension.Module
+{
+    class GameSet
+    {
+        const int CenterRowNum = 6;
+
+        private ArrayList players;
+        private ArrayList centerArea;
+        private Area mysticArea;
+        private Area infantryArea;
+        private Area cultistArea;
+
+        private Area centerDeckArea;
+        private Area voidArea;
+
+        public ArrayList Players 
+        {
+            get
+            {
+                return players;
+            }
+        }
+
+        public ArrayList CenterArea
+        {
+            get
+            {
+                return centerArea;
+            }
+        }
+
+        public Area MysticArea
+        {
+            get
+            {
+                return mysticArea;
+            }
+        }
+
+        public Area InfantryArea
+        {
+            get
+            {
+                return infantryArea;
+            }
+        }
+
+        public Area CultistArea
+        {
+            get
+            {
+                return cultistArea;
+            }
+        }
+
+        public Area CenterDeckArea
+        {
+            get
+            {
+                return centerDeckArea;
+            }
+        }
+
+        public Area VoidArea
+        {
+            get
+            {
+                return voidArea;
+            }
+        }
+
+        public int PlayerNum
+        {
+            get
+            {
+                return players.Count;
+            }
+        }
+
+        public GameSet()
+        {
+            players = new ArrayList();
+            centerArea = new ArrayList();
+            for (int i = 1; i <= CenterRowNum; i++)
+            {
+                centerArea.Add(new Area("中央牌区" + i.ToString()));
+            }
+            mysticArea = new Area("秘教士");
+            infantryArea = new Area("重装步兵");
+            cultistArea = new Area("邪教徒");
+            centerDeckArea = new Area("中央牌库");
+            voidArea = new Area("虚空区");
+        }
+
+        public void AddPlayer (Player player)
+        {
+            players.Add(player);
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            players.Remove(player);
+        }
+    }
+}
--- Module/Player.cs
+++ Module/Player.cs
@@ -7,19 +7,59 @@ namespace Ascension.Module
 {
     class Player
     {
-        string name;
+        private string name;
 
         private Area discardArea;
         private Area handArea;
         private Area deckArea;
 
-        public Player()
+        public Player(string name)
         {
-            discardArea = new Area("弃牌区");
-            handArea = new Area("手牌区");
-            deckArea = new Area("牌组区");
+            this.name = name;
+            discardArea = new Area(name+"的弃牌区");
+            handArea = new Area(name+"的手牌区");
+            deckArea = new Area(name+"的牌组区");
         }
 
-        
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Area DiscardArea
+        {
+            get
+            {
+                return discardArea;
+            }
+        }
+
+        public Area HandArea
+        {
+            get
+            {
+                return handArea;
+            }
+        }
+
+        public Area DeckArea
+        {
+            get
+            {
+                return deckArea;
+            }
+        }
+
+        public override string ToString()
+        {
+            return name + "{" + deckArea + " " + handArea + " " + discardArea + "}";
+        }
     }
 }
