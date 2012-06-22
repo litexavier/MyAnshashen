2ee3caa3851fbaabe0fdde6fb5f526d5943503c6
livingroom <livingroom@qq.com>
2012-06-22 19:51:12 +0800

    添加network

---
+++ Network/NetworkHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace AppTest.Network
+{
+    class NetworkHelper
+    {
+        /// <summary>
+        /// 获取本机ipv4的ip
+        /// </summary>
+        /// <returns>ip</returns>
+        public static string GetIP()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ad in ipHost.AddressList)
+            {
+                if (ad.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ad.ToString();
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 序列化 对象到字符串
+        /// </summary>
+        /// <param name="obj">泛型对象</param>
+        /// <returns>序列化后的字符串</returns>
+        public static byte[] Serialize<T>(T obj)
+        {
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+                MemoryStream stream = new MemoryStream();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                stream.Flush();
+                stream.Close();
+                //return Convert.ToBase64String(buffer);
+                return buffer;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("序列化失败,原因:" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 反序列化 字符串到对象
+        /// </summary>
+        /// <param name="obj">泛型对象</param>
+        /// <param name="str">要转换为对象的字符串</param>
+        /// <returns>反序列化出来的对象</returns>
+        public static T Desrialize<T>(T obj, byte[] buffer)
+        {
+            try
+            {
+                obj = default(T);
+                IFormatter formatter = new BinaryFormatter();
+                //byte[] buffer = Convert.FromBase64String(str);
+                MemoryStream stream = new MemoryStream(buffer);
+                obj = (T)formatter.Deserialize(stream);
+                stream.Flush();
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("反序列化失败,原因:" + ex.Message);
+            }
+            return obj;
+        }
+
+    }
+}
